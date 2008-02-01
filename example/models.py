f610560b1fd1d4091aac73f7bd388eefbf617f9d
Arne Brodowski <arne@rcs4u.de>
2008-02-01 16:45:26 +0000

    forgot the example app ...
    git-svn-id: https://django-rcsfield.googlecode.com/svn/trunk@8 d91cc5cc-1343-0410-b1ed-e1360c4e4ba3

---
+++ example/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from rcsfield.fields import VersionedTextField
+from rcsfield.manager import RevisionManager
+from rcsfield.models import VersionizedModelMixIn
+
+class Entry(models.Model, VersionizedModelMixIn):
+    slug = models.SlugField()
+    text = VersionedTextField(svn_path='/test/', blank=True, null=True)
+    
+    objects = RevisionManager()
+    
+    def __str__(self):
+        return self.slug
+        
+    class Admin:
+        pass
+    
