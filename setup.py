f7723ca05d092e40aef551380c4b7a276fb8760b
Arne Brodowski <arne@rcs4u.de>
2008-05-04 10:17:53 +0000

    added setup.py and other packaging stuff
    git-svn-id: https://django-rcsfield.googlecode.com/svn/trunk@33 d91cc5cc-1343-0410-b1ed-e1360c4e4ba3

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='rcsfield',
+      version='0.1',
+      description='A versionized model field for django',
+      author='Arne Brodowski',
+      author_email='mail@arnebrodowski.de',
+      url='http://code.google.com/p/django-rcsfield/',
+      packages=['rcsfield', 'rcsfield.templatetags'],
+      package_dir={'rcsfield': 'rcsfield'},
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
