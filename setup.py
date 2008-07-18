52a40d6101abad733eee75535905917fad8e9603
Arne Brodowski <arne@rcs4u.de>
2008-07-18 09:23:30 +0000

    added backends directory to setup.py and raised version number to 0.2, a new packaed release will be made available soon.
    git-svn-id: https://django-rcsfield.googlecode.com/svn/trunk@42 d91cc5cc-1343-0410-b1ed-e1360c4e4ba3

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='rcsfield',
-      version='0.1',
+      version='0.2',
       description='A versionized model field for django',
       author='Arne Brodowski',
       author_email='mail@arnebrodowski.de',
       url='http://code.google.com/p/django-rcsfield/',
-      packages=['rcsfield', 'rcsfield.templatetags'],
+      packages=['rcsfield', 'rcsfield.templatetags', 'rcsfield.backends'],
       package_dir={'rcsfield': 'rcsfield'},
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
