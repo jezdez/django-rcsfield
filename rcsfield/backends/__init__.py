3ff6ea3a5e085d0cb87d1623c1bed6380df50b97
Jannis Leidel <jannis@leidel.info>
2008-09-03 16:58:39 +0000

    made backend loading mechanism a bit more robust
    git-svn-id: https://django-rcsfield.googlecode.com/svn/trunk@57 d91cc5cc-1343-0410-b1ed-e1360c4e4ba3

--- rcsfield/backends/__init__.py
+++ rcsfield/backends/__init__.py
@@ -5,27 +5,25 @@ from django.core.exceptions import ImproperlyConfigured
 
 __all__ = ('backend')
 
-if not settings.RCS_BACKEND:
-    settings.RCS_BACKEND = 'dummy'
-    
+RCS_BACKEND = getattr(settings, 'RCS_BACKEND', 'dummy')
+
 try:
     # Try to import one of the bundled backends
     _import_path = 'rcsfield.backends.'
-    backend = __import__('%s%s' % (_import_path, settings.RCS_BACKEND), {}, {}, [''])
+    backend = __import__('%s%s' % (_import_path, RCS_BACKEND), {}, {}, [''])
 except ImportError, e:
     # If the above import failed try to load an external backend
     try:
         _import_path = ''
-        backend = __import__('%s%s' % (_import_path, settings.RCS_BACKEND), {}, {}, [''])
+        backend = __import__('%s%s' % (_import_path, RCS_BACKEND), {}, {}, [''])
     except ImportError, e_user:
         # No backend found, display an error message and a list of all
         # bundled backends.
         backend_dir = __path__[0]
         available_backends = [f.split('.py')[0] for f in os.listdir(backend_dir) if not f.startswith('_') and not f.startswith('.') and not f.endswith('.pyc')]
         available_backends.sort()
-        if settings.RCS_BACKEND not in available_backends:
+        if RCS_BACKEND not in available_backends:
             raise ImproperlyConfigured("%s isn't an available revision control (rcsfield) backend. Available options are: %s" % \
-                                        (settings.RCS_BACKEND, ', '.join(map(repr, available_backends))))
+                                        (RCS_BACKEND, ', '.join(map(repr, available_backends))))
         else:
             raise
-    
